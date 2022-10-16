3dcbd9e41d4fc449259c52dcf7e711a6e1bffa46
Ghulam Mustafa Saivra <info@saivra.com>
2022-10-16 13:25:08 +0500

    database config file update for server

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
 
 /* Configuration of the site */
-define('DATABASE_SERVER',   '');
-define('DATABASE_USERNAME', '');
-define('DATABASE_PASSWORD', '');
-define('DATABASE_NAME',     '');
-define('SITE_URL',          '');
+define('DATABASE_SERVER',   'localhost');
+define('DATABASE_USERNAME', 'bobplaye_standard');
+define('DATABASE_PASSWORD', 'bobplaye_standard');
+define('DATABASE_NAME',     'bobplaye_ust');
+define('SITE_URL',          'https://ultrasmarttools.com/');
 
 
 // <?php
