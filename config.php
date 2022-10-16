143b3d6a51136c078086b0127c62f9f43d78419b
Ghulam Mustafa Saivra <info@saivra.com>
2022-10-16 13:41:07 +0500

    changes

--- config.php
+++ config.php
@@ -1,18 +1,20 @@
 <?php
 
 /* Configuration of the site */
-define('DATABASE_SERVER',   'localhost');
-define('DATABASE_USERNAME', 'bobplaye_standard');
-define('DATABASE_PASSWORD', 'bobplaye_standard');
-define('DATABASE_NAME',     'bobplaye_ust');
-define('SITE_URL',          'https://ultrasmarttools.com/');
 
-
-// <?php
-
-// /* Configuration of the site */
 // define('DATABASE_SERVER',   'localhost');
-// define('DATABASE_USERNAME', 'root');
-// define('DATABASE_PASSWORD', '');
-// define('DATABASE_NAME',     'tools');
-// define('SITE_URL',          'http://localhost/product/');
+// define('DATABASE_USERNAME', 'bobplaye_standard');
+// define('DATABASE_PASSWORD', 'bobplaye_standard');
+// define('DATABASE_NAME',     'bobplaye_ust');
+// define('SITE_URL',          'https://ultrasmarttools.com/');
+
+/* Configuration of the site */
+
+define('DATABASE_SERVER',   'localhost');
+define('DATABASE_USERNAME', 'root');
+define('DATABASE_PASSWORD', '');
+define('DATABASE_NAME',     'tools');
+define('SITE_URL',          'http://localhost/product/');
+
+
+
