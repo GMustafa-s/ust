6388d3bb6041b0866b1719ce2317df650f4747fe
Ghulam Mustafa Saivra <info@saivra.com>
2022-10-16 13:16:31 +0500

    config

--- config.php
+++ config.php
@@ -1,8 +1,18 @@
 <?php
 
 /* Configuration of the site */
-define('DATABASE_SERVER',   'localhost');
-define('DATABASE_USERNAME', 'root');
+define('DATABASE_SERVER',   '');
+define('DATABASE_USERNAME', '');
 define('DATABASE_PASSWORD', '');
-define('DATABASE_NAME',     'tools');
-define('SITE_URL',          'http://localhost/product/');
+define('DATABASE_NAME',     '');
+define('SITE_URL',          '');
+
+
+// <?php
+
+// /* Configuration of the site */
+// define('DATABASE_SERVER',   'localhost');
+// define('DATABASE_USERNAME', 'root');
+// define('DATABASE_PASSWORD', '');
+// define('DATABASE_NAME',     'tools');
+// define('SITE_URL',          'http://localhost/product/');
